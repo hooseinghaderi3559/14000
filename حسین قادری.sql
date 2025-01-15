754d22c171eacca071b846c45c671ced505fd62e
Hosseinghaderi3559 <qadryh30@gmail.com>
2025-01-16 00:58:36 +0330

    Add files via upload

---
+++ حسین قادری.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Products(
+ProductID INT PRIMARY KEY,
+ProductName VARCHAR(100),
+Price DECIMAL(10,2)
+);
+ INSERT INTO Products (ProductID,ProductName,Price) VALUES
+(1, 'Laptop',999.99),
+(2, 'smartphon',699.99),
+(3, 'tabiet', 399.99);
